da9768c0fd5dbefb831a78a64ea84445b40119e0
Fayz <fayz.siddiqui@outlook.com>
2021-11-03 17:57:19 +0000

    Pushing final files

--- src/main/java/shield/MyImpUtils.java
+++ src/main/java/shield/MyImpUtils.java
@@ -6,23 +6,30 @@ import java.util.Locale;
 import java.util.Random;
 
 
+
 public class MyImpUtils {
+
+    /**
+     * Checks validity of postcode
+     * @param postcode String
+     * @return true if postcode passes all checks, else returns false
+     */
     public static boolean checkValidPostcode (String postcode) {
         if (postcode.length() == 7) {
             return postcode.toUpperCase(Locale.ROOT).startsWith("EH") &&
-                    Character.isDigit(postcode.charAt(2)) &&
-                    postcode.charAt(3) == '_' &&
-                    Character.isDigit(postcode.charAt(4)) &&
-                    Character.isAlphabetic(postcode.charAt(5)) &&
-                    Character.isAlphabetic(postcode.charAt(6));
+                       Character.isDigit(postcode.charAt(2)) &&
+                       postcode.charAt(3) == '_' &&
+                       Character.isDigit(postcode.charAt(4)) &&
+                       Character.isAlphabetic(postcode.charAt(5)) &&
+                       Character.isAlphabetic(postcode.charAt(6));
 
         } else if (postcode.length() == 8) {
             return postcode.toUpperCase(Locale.ROOT).startsWith("EH") &&
-                    postcode.substring(2, 4).matches("[0-9]+") &&
-                    postcode.charAt(4) == '_' &&
-                    Character.isDigit(postcode.charAt(5)) &&
-                    Character.isAlphabetic(postcode.charAt(6)) &&
-                    Character.isAlphabetic(postcode.charAt(7));
+                       postcode.substring(2, 4).matches("[0-9]+") &&
+                       postcode.charAt(4) == '_' &&
+                       Character.isDigit(postcode.charAt(5)) &&
+                       Character.isAlphabetic(postcode.charAt(6)) &&
+                       Character.isAlphabetic(postcode.charAt(7));
         } else return false;
     }
 }
